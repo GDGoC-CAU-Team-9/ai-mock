ec0274080fe0b8c3df4446b4878170465a2267be
cau20232907 <hs20232907@cau.ac.kr>
2026-04-10 23:52:51 +0900

    Initial commit

---
+++ src/main/java/com/gdg_team9/aimock/AimockApplication.java
@@ -0,0 +1,13 @@
+package com.gdg_team9.aimock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AimockApplication.class, args);
+    }
+
+}
